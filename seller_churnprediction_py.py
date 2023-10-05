b28aa6339c81ade10fe49f2df93b63ddcff7dd8a
evliya-udayev <314768@stud.umk.pl>
2023-10-05 12:08:49 +0200

    Update seller_churnprediction_py.py

--- seller_churnprediction_py.py
+++ seller_churnprediction_py.py
@@ -143,6 +143,7 @@ import shap
 import streamlit_shap
 from streamlit_shap import st_shap
 from io import BytesIO
+import openpyxl
 # Load your trained model
 #model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=5,
                                    #min_samples_leaf=7, min_samples_split=2, random_state=42)
